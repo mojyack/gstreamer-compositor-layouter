0993faa0362b41de28babd6a42a114321e6895a3
mojyack <mojyack@gmail.com>
2024-05-20 16:41:51 +0900

    example: remove elements

--- src/example.cpp
+++ src/example.cpp
@@ -10,72 +10,85 @@
 #include "macros/autoptr.hpp"
 #include "macros/unwrap.hpp"
 #include "util/assert.hpp"
+#include "util/event.hpp"
 
 namespace {
 declare_autoptr(GString, gchar, g_free);
 declare_autoptr(GstMessage, GstMessage, gst_message_unref);
 declare_autoptr(GstCaps, GstCaps, gst_caps_unref);
 
-auto compositor_pad_added_handler(GstElement* const /*compositor*/, GstPad* const pad, gpointer const /*data*/) -> void {
-    const auto name_g = AutoGString(gst_object_get_name(GST_OBJECT(pad)));
-    const auto name   = std::string_view(name_g.get());
-
-    PRINT("pad added name=", name);
-}
-
-auto compositor_pad_removed_handler(GstElement* const /*compositor*/, GstPad* const pad, gpointer const /*data*/) -> void {
-    const auto name_g = AutoGString(gst_object_get_name(GST_OBJECT(pad)));
-    const auto name   = std::string_view(name_g.get());
-    PRINT("pad removed name=", name);
-}
-
 auto manage() -> bool {
     const auto pipeline = AutoGstObject(gst_pipeline_new(NULL));
     assert_b(pipeline.get() != NULL);
 
+    const auto waylandsink_caps = AutoGstCaps(gst_caps_new_simple("video/x-raw",
+                                                                  "width", G_TYPE_INT, 800,
+                                                                  "height", G_TYPE_INT, 600,
+                                                                  NULL));
+
     unwrap_pb_mut(compositor, add_new_element_to_pipeine(pipeline.get(), "compositor"));
     unwrap_pb_mut(videoconvert, add_new_element_to_pipeine(pipeline.get(), "videoconvert"));
+    unwrap_pb_mut(capsfilter, add_new_element_to_pipeine(pipeline.get(), "capsfilter"));
+    g_object_set(&capsfilter, "caps", waylandsink_caps.get(), NULL);
     unwrap_pb_mut(waylandsink, add_new_element_to_pipeine(pipeline.get(), "waylandsink"));
+    g_object_set(&waylandsink, "async", FALSE, NULL);
+
+    assert_b(gst_element_link_pads(&compositor, NULL, &videoconvert, NULL) == TRUE);
+    assert_b(gst_element_link_pads(&videoconvert, NULL, &capsfilter, NULL) == TRUE);
+    assert_b(gst_element_link_pads(&capsfilter, NULL, &waylandsink, NULL) == TRUE);
 
     auto layouter    = CompositorLayouter(&compositor);
     layouter.verbose = true;
 
-    g_object_set(&waylandsink, "async", FALSE, NULL);
-    g_signal_connect(G_OBJECT(&compositor), "pad-added", G_CALLBACK(compositor_pad_added_handler), &layouter);
-    g_signal_connect(G_OBJECT(&compositor), "pad-removed", G_CALLBACK(compositor_pad_removed_handler), &layouter);
+    auto worker = std::thread([&pipeline, &layouter]() -> bool {
+        constexpr auto max_sources = 16;
+        constexpr auto delay       = std::chrono::milliseconds(500);
 
-    assert_b(gst_element_link_pads(&compositor, NULL, &videoconvert, NULL) == TRUE);
-    assert_b(gst_element_link_pads(&videoconvert, NULL, &waylandsink, NULL) == TRUE);
+        auto sources = std::array<CompositorLayouter::Source*, max_sources>();
 
-    auto w = std::thread([&pipeline, &layouter]() {
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-        unwrap_pb_mut(videotestsrc1, add_new_element_to_pipeine(pipeline.get(), "videotestsrc"));
-        unwrap_pb(src1, layouter.add_src(gst_element_get_static_pad(&videotestsrc1, "src")));
+        // add sources
+        for(auto& source : sources) {
+            unwrap_pb_mut(videotestsrc, add_new_element_to_pipeine(pipeline.get(), "videotestsrc"));
+            unwrap_pb_mut(src, layouter.add_src(gst_element_get_static_pad(&videotestsrc, "src")));
+            assert_b(gst_element_sync_state_with_parent(&videotestsrc) == TRUE);
+            source = &src;
+            std::this_thread::sleep_for(delay);
+        }
 
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-        unwrap_pb_mut(videotestsrc2, add_new_element_to_pipeine(pipeline.get(), "videotestsrc"));
-        unwrap_pb(src2, layouter.add_src(gst_element_get_static_pad(&videotestsrc2, "src")));
+        std::this_thread::sleep_for(delay);
 
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-        unwrap_pb_mut(videotestsrc3, add_new_element_to_pipeine(pipeline.get(), "videotestsrc"));
-        unwrap_pb(src3, layouter.add_src(gst_element_get_static_pad(&videotestsrc3, "src")));
+        // removing an element from a live pipeline is hard...
+        auto delete_ready      = Event();
+        auto element_to_delete = (GstElement*)(nullptr);
+        auto delete_element    = [&delete_ready, &element_to_delete](GstPad* const pad) {
+            assert_n(element_to_delete == nullptr, "unexpected element removal");
+            unwrap_pn_mut(element, gst_pad_get_parent_element(pad));
+            // we cannot delete element here.
+            // because this function maybe called from its streaming thread.
+            element_to_delete = &element;
+            delete_ready.wakeup();
+        };
 
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-        unwrap_pb_mut(videotestsrc4, add_new_element_to_pipeine(pipeline.get(), "videotestsrc"));
-        unwrap_pb(src4, layouter.add_src(gst_element_get_static_pad(&videotestsrc4, "src")));
+        // remove elements
+        for(auto& source : sources) {
+            layouter.remove_src(source, delete_element);
+            delete_ready.wait();
+            delete_ready.clear();
 
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-        layouter.remove_src(&src4, {});
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-        layouter.remove_src(&src3, {});
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-        layouter.remove_src(&src2, {});
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-        layouter.remove_src(&src1, {});
+            assert_b(gst_bin_remove(GST_BIN(pipeline.get()), element_to_delete) == TRUE);
+            assert_b(gst_element_set_state(element_to_delete, GST_STATE_NULL) == GST_STATE_CHANGE_SUCCESS);
+            gst_object_unref(element_to_delete);
+            element_to_delete = nullptr;
+
+            std::this_thread::sleep_for(delay);
+        }
+
         return true;
     });
 
-    return run_pipeline(pipeline.get());
+    const auto ret = run_pipeline(pipeline.get());
+    worker.join();
+    return ret;
 }
 } // namespace
 
